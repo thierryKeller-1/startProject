2a5f758966272079d35e15a269663356b63222a8
thierryKeller-1 <thierry0keller@gmail.com>
2022-05-23 14:06:04 +0300

    change form

--- LMS/soluAcademie/users/views.py
+++ LMS/soluAcademie/users/views.py
@@ -1,13 +1,29 @@
+from pyexpat import model
+from typing import Any, Dict
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render
 
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse
 
 from .models import SLMUser
-from .forms import RegistrationForm
+from .forms import UserRegistrationForm
+
+
+class UserCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    model = SLMUser
+    form_class = UserRegistrationForm
+
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+
+        
+
+        context =  super().get_context_data(**kwargs)
+
+        return context
