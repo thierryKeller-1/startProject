9f927aa440e2976859bc47ff596d671f296679aa
thierryKeller-1 <thierry0keller@gmail.com>
2022-05-23 15:09:25 +0300

    adding last change

--- LMS/soluAcademie/users/views.py
+++ LMS/soluAcademie/users/views.py
@@ -1,5 +1,3 @@
-from pyexpat import model
-from typing import Any, Dict
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout
 from django.contrib import messages
@@ -18,12 +16,14 @@ from .forms import UserRegistrationForm
 class UserCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = SLMUser
     form_class = UserRegistrationForm
+    # template_name: str('addUser.html')
 
+    def get_context_data(self, **kwargs):
 
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-
-        
+        # adding list cours here
 
         context =  super().get_context_data(**kwargs)
+
+        user = self.request.user
 
         return context
